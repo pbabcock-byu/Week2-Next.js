ec9c9b545d1edf8fd3c903e1adfe10b1ef7d8939
Peter <peterb@saefl.com>
2025-07-18 19:21:51 -0700

    ch 11 Complete Adding Search and Pagination

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+// import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -9,18 +10,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    console.log(`Searching... ${term}`);
+    //const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
 
-    const params = new URLSearchParams(searchParams);
-    // this send whats in the search window to the console // out before production
-    //console.log(term);
-    
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+      const params = new URLSearchParams(searchParams);
+     
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    //}, 300);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
