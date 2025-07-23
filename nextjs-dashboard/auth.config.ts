b5a9a0751ce9d053af3a2c57d45c15b4e2fd8d45
Peter <peterb@saefl.com>
2025-07-22 23:17:57 -0700

    Update changes

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -4,24 +4,18 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  callbacks: {
-    //authorized({ auth, request: { nextUrl } }) {
-    authorized({ auth, request }: { auth: any, request: { nextUrl: any } }) {
-      const { nextUrl } = request;
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      /*if (isOnDashboard) {
-        return isLoggedIn;
-      }*/
-
-      return true;
-    },
+callbacks: {
+  authorized({ auth, request: { nextUrl } }) {
+    const isLoggedIn = !!auth?.user;
+    const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+    if (isOnDashboard) {
+      if (isLoggedIn) return true;
+      return false; // Redirect unauthenticated users to login page
+    } else if (isLoggedIn) {
+      return Response.redirect(new URL('/dashboard', nextUrl));
+    }
+    return true;
   },
-  providers: [], // Add providers with an empty array for now
+},
+providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
